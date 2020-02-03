618370babd8965ae2bef7064cc28274c57c35939
Hayoun Lee <sidhlee@gmail.com>
2020-02-03 22:39:00 +0900

    Initial commit
    Mock App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Slices</div>
+      <div>SlideNav</div>
+      <div>Controls</div>
+      <div>ProgressBar</div>
+      <div>
+        This is some accessibility features for SR
+        <h3>VisuallyHidden</h3>
+        <div>Alert></div>
+      </div>
     </div>
   );
 }
