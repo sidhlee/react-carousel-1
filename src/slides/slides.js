1d99174fd5002eafd3c6713c3d1ccef182223382
Hayoun Lee <sidhlee@gmail.com>
2020-02-04 20:09:41 +0900

    Add metadata for images

--- src/slides/slides.js
+++ src/slides/slides.js
@@ -14,11 +14,53 @@ const images = importAll(
   require.context("./", false, /.*.jpg$/i)
 );
 
+const imgData = [
+  {
+    id: "img0",
+    title: "Brother and Sister",
+    content:
+      "Ethan and Elaine are watching Pokemon together on a bunker bed.",
+    position: "top"
+  },
+  {
+    id: "img1",
+    title: "My ball!",
+    content:
+      "Dad and Ethan are kicking some balls at Youido Park under the blue sky.",
+    position: "top"
+  },
+  {
+    id: "img2",
+    title: "Sunshine Girl",
+    content: "Elaine strolling down in her pretty shoes.",
+    position: "bottom"
+  },
+  {
+    id: "img3",
+    title: "Here goes the cannon ball!",
+    content: "Ethan is ready to launch his final attack on dad.",
+    position: "top"
+  },
+  {
+    id: "img4",
+    title: "Side by Side",
+    content: "A sunny afternoon at Han-River Park",
+    position: "bottom"
+  },
+  {
+    id: "img5",
+    title: "Good Morning Yellow Brick Road",
+    content: "Ethan is walking to the Kindergarten with mom and dad.",
+    position: "top"
+  }
+];
+
 const imgs = Object.entries(images).map(([key, value], index) => {
   return {
     img: value,
-    title: "Happy Day",
-    content: <p>Cafe MonJube</p>
+    title: imgData[index].title,
+    content: <p>{imgData[index].content}</p>,
+    position: imgData[index].position
   };
 });
 
